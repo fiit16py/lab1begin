adb7c5de06c2e7cd1ae6c297e3f0b99941dfe062
Dmitriev Mikhail <43399829+Ducktavius@users.noreply.github.com>
2019-10-02 15:37:00 +0900

    task 3 done

--- task3.py
+++ task3.py
@@ -1,5 +1,13 @@
 def is_beautiful(n):
-	pass
+        sum = 0
+        m = n
+        while (n > 0):
+                sum += n % 10
+                n = n // 10
+        if (m % sum == 0):
+                print('true')
+        else:
+                print('false')
 
 n = int(input())
 print(is_beautiful(n))
