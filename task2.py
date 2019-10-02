6244ef690919333d8505b0205f025b68969c2ecc
Студент <AlbertoMalvius@users.noreply.github.com>
2019-10-02 15:17:45 +0900

    Task1 Petrov Egor

--- task2.py
+++ task2.py
@@ -1,6 +1,12 @@
 def digit_sum(n):
-    pass
+    a=n/10
+    n/10
+    while n/10!=0:
+        a=a+n/10
+        n/10
+    print(a)
 
 print('Введите число')
 n = int(input())
+print('123')
 print(digit_sum(n))
