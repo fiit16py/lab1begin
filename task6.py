906ce8fc78acb1502797aec9db6d41fb63580e90
platonov simon <plats2002@gmail.com>
2019-10-05 16:10:21 +0900

    Task6 done

--- task6.py
+++ task6.py
@@ -1,5 +1,14 @@
 def get_largest_perimiter(L):
-	pass
+    l = list(L)
+    s = 0
+    for a in range(len(l) - 2):
+        for b in range(a + 1, len(l) - 1):
+            for c in range(b + 1, len(l)):
+                if l[a] + l[b] > l[c] and l[a] + l[c] > l[b] and l[b] + l[c] > l[a]:
+                    if s < (l[a] + l[b] + l[c]):
+                        s = l[a] + l[b] + l[c]
+    return s
+
 
 L = map(int, input().split())
 print(get_largest_perimiter(L))
