91858cabbb23e488ffd9f25988981498b8efd90f
platonov simon <plats2002@gmail.com>
2019-10-05 16:09:25 +0900

    Task5 done

--- task5.py
+++ task5.py
@@ -1,5 +1,18 @@
 def is_prime(n):
-	pass
+	if n < 2:
+		return False
+	elif n == 2:
+		return True
+	elif n % 2 == 0:
+		return False
+	else:
+		s = 2
+		while s < n:
+			if n % s == 0:
+				return False
+			s += 1
+		return True
+
 
 n = int(input())
 print(is_prime(n))
