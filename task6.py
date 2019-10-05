b84efac79da08ec8402c167b3fc55b838a5dfec1
LATRio <AlSenya@yandex.ru>
2019-10-05 13:48:07 +0900

    Update task6.py

--- task6.py
+++ task6.py
@@ -1,5 +1,21 @@
 def get_largest_perimiter(L):
-	pass
+    n = len(L)
+    if n < 3:
+        return 0
+    mx = 0
+    x = 0
+    y = 1
+    z = 2
+    for x in range(n - 2):
+        for y in range(x + 1, n - 1):
+            for z in range(y + 2, n):
+                a = L[x]
+                b = L[y]
+                c = L[z]
+                if a + b < c or a + c < b or c + b < a:
+                    continue
+                mx = max(mx, a + b + c)
+    return mx
 
-L = map(int, input().split())
+L = [int(i) for i in input().split()]
 print(get_largest_perimiter(L))
