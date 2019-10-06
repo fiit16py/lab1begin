95ffacca7bc986328c54324beff3430a4fec3b07
arsen andreev <zeroyak@gmail.com>
2019-10-06 19:43:24 +0900

    Done task3

--- task3.py
+++ task3.py
@@ -1,5 +1,15 @@
 def is_beautiful(n):
-	pass
+        a = 0
+        z = n
+        while(n > 0):
+                a = a + n % 10
+                n = n // 10
+        if z % a == 0:
+                return True
+        else:
+                return False
+n = 1
 
-n = int(input())
-print(is_beautiful(n))
+while(n != 999):
+        n = int(input())
+        print(is_beautiful(n))
