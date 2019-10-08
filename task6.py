4adb3cff8b19caa484bd0fa702ca56ee247d8541
arsen andreev <zeroyak@gmail.com>
2019-10-08 22:48:43 +0900

    Done task6

--- task6.py
+++ task6.py
@@ -5,15 +5,15 @@ def check_triangle(a, b, c):
                 return False
 
 def get_largest_perimiter(L):
-    l = list(L)
-    P = 0
-    for a in range(len(l)):
-        for b in range(a + 1, len(l)):
-            for c in range(b + 1, len(l)):
-                if check_triangle(l[a], l[b], l[c]):
-                    if P < (l[a] + l[b] + l[c]):
-                        P = l[a] + l[b] + l[c]
-    return P
+        l = list(L)
+        P = 0
+        for a in range(len(l)):
+                for b in range(a + 1, len(l)):
+                        for c in range(b + 1, len(l)):
+                                if check_triangle(l[a], l[b], l[c]):
+                                        if P < (l[a] + l[b] + l[c]):
+                                                P = l[a] + l[b] + l[c]
+        return P
 
 L = map(int, input().split())
 print(get_largest_perimiter(L))
