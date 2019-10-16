f13cba2ae4a7611edfb637bf351836d9d8413ba3
Кыппыгыров Сарыал Васильевич <swofde@MacBook-Pro-Kyppygyrov.local>
2019-10-16 16:42:56 +0900

    task6

--- task6.py
+++ task6.py
@@ -1,5 +1,13 @@
 def get_largest_perimiter(L):
-	
-
-L = map(int, input().split())
+    largest_per = -1
+    for i in range(len(L) - 2):
+        for j in range(i, len(L) - 1):
+            for k in range(j, len(L)):
+                a = L[i]
+                b = L[j]
+                c = L[k]
+                if  a + b > c and a + c > b and b + c > a and a + b + c > largest_per:
+                    largest_per = a + b + c
+    return largest_per
+L = list(map(int, input().split()))
 print(get_largest_perimiter(L))
