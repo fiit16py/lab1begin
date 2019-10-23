2a7ed3cd8bcd8326e25205dbaf03c79b5f495f8a
VasilevSaryal <saryal.basilev@mail.ru>
2019-10-23 12:41:20 +0900

    task 5 done

--- task5.py
+++ task5.py
@@ -1,5 +1,12 @@
 def is_prime(n):
-	pass
+	if (n == 1):
+		return "False"
+	i = 2
+	while i <= n**0.5:
+		if (n % i == 0):
+			return "False"
+		i += 1
+	return "True"
 
-n = int(input())
+n = int(input()) 
 print(is_prime(n))
