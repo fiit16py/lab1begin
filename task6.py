9794451e1b1c48618afdf1f8b77dc8bc73212154
VasilevSaryal <saryal.basilev@mail.ru>
2019-10-23 12:55:42 +0900

    task 6 done

--- task6.py
+++ task6.py
@@ -1,5 +1,19 @@
-def get_largest_perimiter(L):
-	pass
+def get_largest_perimiter(arr):
+	maxi = 0
+	n = len(arr) 
+	for i in range(n - 2): 
+		for j in range(i + 1, n - 1): 
+			for k in range(j + 1, n): 
+				a = arr[i] 
+				b = arr[j] 
+				c = arr[k] 
+				if(a < b + c and b < a + c and c < a + b): 
+					maxi = max(maxi, a + b + c) 
 
-L = map(int, input().split())
+	if(maxi == 0): 
+		return "Triangle formation is not possible"
+	else: 
+		return "Maximum Perimeter is: "+ str(maxi) 
+
+L = list(map(int, input().split()))
 print(get_largest_perimiter(L))
