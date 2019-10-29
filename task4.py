4bd7d032497055a7aa02070b49061b2dfc02b763
max xenofontov <ramutra1996@gmail.com>
2019-10-29 12:39:18 +0900

    Done task4 lab1

--- task4.py
+++ task4.py
@@ -1,5 +1,10 @@
 def is_power_of_two(n):
-	pass
+        while(n>1):
+                if(n%2==0):
+                        n=n/2
+                else:
+                        return False
+        return True
 
 n = int(input())
 print(is_power_of_two(n))
