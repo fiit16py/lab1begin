e5ce210ee16a9dff300d22a630f88c6557299bd4
max xenofontov <ramutra1996@gmail.com>
2019-10-29 12:30:17 +0900

    Done task3 lab1

--- task3.py
+++ task3.py
@@ -1,5 +1,14 @@
 def is_beautiful(n):
-	pass
+    s=0
+    a=n
+    while(n//10>0):
+        s=s+n%10
+        n=n//10
+    s=s+n
+    if(a%s==0):
+        return True
+    else:
+        return False
 
 n = int(input())
 print(is_beautiful(n))
