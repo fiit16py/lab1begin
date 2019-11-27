45de125def2a8a65bca236b82df80dd785dd522a
eugeniy <43114426+enuvin@users.noreply.github.com>
2019-11-27 12:26:01 +0900

    upd

--- task6.py
+++ task6.py
@@ -1,6 +1,11 @@
 def get_largest_perimiter(L):
-        print("прокатило")
-pass
-
-L = map(int, input().split())
-print(get_largest_perimiter(L))
+        pmax = 0
+        for i in range(len(L)-2):
+            for j in range(i,len(L)-1):
+                for q in range(j,len(L)):
+                    if L[i] + L[j] + L[q] > pmax:
+                        pmax = L[i] + L[j] + L[q]
+        return pmax
+if __name__ == '__main__':
+        L = list(map(int, input().split()))
+        print(get_largest_perimiter(L))
